af6c2841fb350a046f8298a0af5268674b0fe359
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-05-16 11:48:29 +0200

    Add stats logging

--- tests/testApp/imports/server/lib/setupTests.test.js
+++ tests/testApp/imports/server/lib/setupTests.test.js
@@ -3,7 +3,7 @@ import { initializeMethodCache } from 'meteor/epotek:method-cache';
 import '../methods';
 
 console.log('Starting cache methods');
-initializeMethodCache();
+initializeMethodCache({ logStats: true });
 
 describe('Init', () => {
   before((done) => {
